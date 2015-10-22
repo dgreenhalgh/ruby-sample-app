76ecec31f9652d255ab26b3ac184dcbb6d02593b
David Greenhalgh <davidrgreenhalgh@gmail.com>
2015-10-22 13:37:07 -0400

    Added full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	# Returns full title for each page
+	def full_title(page_title = '')
+		base_title = "RoR Tutorial"
+
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
 end
